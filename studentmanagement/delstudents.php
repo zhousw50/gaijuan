2ca56fcafd3a99d76460796e3f73369b88adb930
周声文 <73733215+zhousw50@users.noreply.github.com>
2022-10-06 01:43:35 +0000

    Delete studentmanagement directory

--- studentmanagement/delstudents.php
+++
@@ -1,7 +0,0 @@
-<?php
-$link=new PDO("mysql:host=localhost;dbname=zhousw","zhousw","qwerty");
-$id=$_POST["id"];
-$prepare=$link->prepare("delete from students where id=?;");
-$prepare->execute(array($id));
-echo "-->操作成功<--";
-?>
